64ee669c3bbb6a9d3dfa9beb7a23d877f230aa0a
xiemin <xiemin@farmfriend.cn>
2018-08-24 06:33:16 +0800

    修改变量名称

--- src/main/java/com/imgeek/algorithm/MergeSort.java
+++ src/main/java/com/imgeek/algorithm/MergeSort.java
@@ -4,54 +4,55 @@ import java.lang.reflect.Array;
 
 /**
  * 归并排序
+ *
  * @author HongMong
  * @date 2018-08-08
  */
-public class MergeSort extends MySort{
+public class MergeSort extends MySort {
 
     public MergeSort(Sort sort) {
         super(sort);
     }
 
-    public void sort(int[] ints){
-        mergeSort(ints,0,ints.length-1);
+    public void sort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
     }
 
-    private void mergeSort(int[] ints,int low,int high){
+    private void mergeSort(int[] ints, int low, int high) {
         int middle = (low + high) / 2;
-        if(low < high){
+        if (low < high) {
 
-           mergeSort(ints,low ,middle);
+            mergeSort(ints, low, middle);
 
-           mergeSort(ints,middle + 1,high);
+            mergeSort(ints, middle + 1, high);
 
-           merge(ints,low,middle,high);
+            merge(ints, low, middle, high);
         }
     }
 
-    private void merge(int[] ints,int low,int middle,int high){
+    private void merge(int[] arr, int low, int middle, int high) {
         int[] temp = new int[high - low + 1];
         int _left = low;
         int _right = middle + 1;
         int index = 0;
-        while (_left <= middle && _right <= high){
+        while (_left <= middle && _right <= high) {
 
-            if(compare(ints[_left],ints[_right])){
-                temp[index++] = ints[_left++];
-            }else {
-                temp[index++] = ints[_right++];
+            if (compare(arr[_left], arr[_right])) {
+                temp[index++] = arr[_left++];
+            } else {
+                temp[index++] = arr[_right++];
             }
         }
-        while (_left <= middle){
-            temp[index++] = ints[_left++];
+        while (_left <= middle) {
+            temp[index++] = arr[_left++];
         }
 
-        while (_right <= high){
-            temp[index++] = ints[_right++];
+        while (_right <= high) {
+            temp[index++] = arr[_right++];
         }
 
-        for(int i : temp){
-            ints[low++] = i;
+        for (int i : temp) {
+            arr[low++] = i;
         }
     }
 }
