940cb342039cc541aab5a21beda4a6a125ea23f6
xiemin <xiemin@farmfriend.cn>
2018-09-16 22:14:39 +0800

    二分查找再优化哈哈@wangyufeng

--- src/main/java/com/imgeek/algorithm/HalfFind.java
+++ src/main/java/com/imgeek/algorithm/HalfFind.java
@@ -11,10 +11,16 @@ public class HalfFind {
     private int sorted = 0;
 
     public HalfFind(int sorted) {
-        sorted = 0; //sorted ：0 正序 sorted ：1 逆序
+        this.sorted = sorted; //sorted ：0 正序 sorted ：1 逆序
     }
 
     public int find(int[] arr, int key) {
+        /**
+         * 判断是否是有序数组
+         */
+        if (!isSorted(arr, 0, arr.length - 1)) {
+            throw new NumberFormatException("not sorted array");
+        }
         return find(arr, key, 0, arr.length - 1);
     }
 
@@ -26,12 +32,7 @@ public class HalfFind {
      * @return
      */
     private int find(int[] arr, int key, int start, int end) {
-        /**
-         * 判断是否是有序数组
-         */
-        if (!isSorted(arr, start, end)) {
-            throw new NumberFormatException("not sorted array");
-        }
+
 
         if (start > end) {
             return -1;
@@ -58,26 +59,8 @@ public class HalfFind {
      * @param arr
      * @return
      */
-    private boolean isSorted(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (sorted == 1 && arr[i] < arr[i + 1]) {
-                return false;
-            }
-            if (sorted == 0 && arr[i] > arr[i + 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * 判断数组是否有序 sorted = 0: 正序 sorted = 1: 逆序
-     *
-     * @param arr
-     * @return
-     */
     private boolean isSorted(int[] arr, int start, int end) {
-        for (int i = start; i < end - 1; i++) {
+        for (int i = start; i < end; i++) {
             if (sorted == 1 && arr[i] < arr[i + 1]) {
                 return false;
             }
